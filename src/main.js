ea6036b254eefd546578f6e8b6bb33f4ad9c6112
lizhixiong <1063357624@qq.com>
2022-07-12 23:22:10 +0800

    test

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vueCesium from 'vue-cesium'
+import 'vue-cesium/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(vueCesium)
+
+app.use(store).use(router).mount('#app')
